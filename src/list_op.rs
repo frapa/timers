e5f4e843c42578eb06ec5199c3ba31644ee9770a
Francesco Pasa <francescopasa@gmail.com>
2020-03-23 19:05:26 +0100

    Use a table to show the tasks

--- src/list_op.rs
+++ src/list_op.rs
@@ -1,64 +1,120 @@
 use std::collections::HashMap;
 
 use colored::*;
-use itertools::Itertools;
+use itertools::{Itertools, enumerate};
+use crate::util::parse_int;
+
+trait TaskPrinter {
+    fn print_header(&self);
+    fn print_ellipsis(&self);
+    fn print_task(&self, task: &timers::Task);
+
+    fn print_tasks(&self, tasks: HashMap<u32, timers::Task>, num: usize) {
+        self.print_header();
+
+        if tasks.len() > num {
+            self.print_ellipsis();
+        }
+
+        for (i, id) in enumerate(tasks.keys().sorted()) {
+            if tasks.len() > num && i < tasks.len()-num {
+                continue
+            }
+
+            let task = &tasks[id];
+            self.print_task(task);
+        }
+    }
+}
+
+struct ShortPrinter ();
+
+impl TaskPrinter for ShortPrinter {
+    fn print_header(&self) {
+        println!("{:<6} {:<36} {}", "ID", "TASK", "DURATION");
+    }
+
+    fn print_ellipsis(&self) {
+        println!("{:<6} {:<36} {}", "...", "...", "...");
+    }
+
+    fn print_task(&self, task: &timers::Task) {
+        match task.status() {
+            timers::TaskStatus::Logging() => println!(
+                "{:<6} {:<36} {}",
+                format!("@{}", task.id).yellow().bold(),
+                task.name.red().bold(),
+                timers::format_duration(task.duration()).bold(),
+            ),
+            timers::TaskStatus::Stopped() => println!(
+                "{:<6} {:<36} {}",
+                format!("@{}", task.id),
+                task.name,
+                timers::format_duration(task.duration()),
+            ),
+        }
+    }
+}
+
+struct LongPrinter ();
+
+impl TaskPrinter for LongPrinter {
+    fn print_header(&self) {
+        println!(
+            "{:<6} {:<36} {:<14} {:<8} {:<6} {}",
+             "ID", "TASK", "DURATION", "STATUS", "LOGS", "LAST LOG"
+        );
+    }
+
+    fn print_ellipsis(&self) {
+        println!(
+            "{:<6} {:<36} {:<14} {:<8} {:<6} {}",
+            "...", "...", "...", "...", "...", "..."
+        );
+    }
+
+    fn print_task(&self, task: &timers::Task) {
+        let last = task.logs.last().unwrap()
+            .start.with_timezone(&chrono::Local)
+            .format("%a %b %d %H:%M").to_string();
+
+        match task.status() {
+            timers::TaskStatus::Logging() => println!(
+                "{:<6} {:<36} {:<14} {:<8} {:<6} {}",
+                format!("@{}", task.id).yellow().bold(),
+                task.name.red().bold(),
+                timers::format_duration(task.duration()).bold(),
+                task.status_text().bold(),
+                format!("{}", task.logs.len()).bold(),
+                last.bold(),
+            ),
+            timers::TaskStatus::Stopped() => println!(
+                "{:<6} {:<36} {:<14} {:<8} {:<6} {}",
+                format!("@{}", task.id),
+                task.name,
+                timers::format_duration(task.duration()),
+                task.status_text(),
+                task.logs.len(),
+                last,
+            ),
+        }
+    }
+}
 
 pub fn tasks_command(matches: &clap::ArgMatches) {
+    let raw_num = matches.value_of("num").unwrap();
+    let num = parse_int(raw_num).unwrap_or_else(
+        |_| {
+            println!("Invalid number of tasks: '{}'", raw_num);
+            std::process::exit(1);
+        }
+    ) as usize;
+
     match timers::get_all_tasks() {
-        Ok(tasks) => {
-            match matches.is_present("long") {
-                true => print_tasks_long(tasks),
-                false => print_tasks(tasks),
-            }
+        Ok(tasks) => match matches.is_present("long") {
+            true => LongPrinter{}.print_tasks(tasks, num),
+            false => ShortPrinter{}.print_tasks(tasks, num),
         },
         Err(err) => println!("Error retrieving tasks: {}", err)
-    }
-}
-
-fn print_tasks(tasks: HashMap<u32, timers::Task>) {
-    for id in tasks.keys().sorted() {
-        let task = &tasks[id];
-        match task.status() {
-            timers::TaskStatus::Logging() => println!(
-                "{} {} [{}]",
-                format!("@{}:", task.id).yellow().bold(),
-                task.name.red().bold(),
-                task.status_text(),
-            ),
-            timers::TaskStatus::Stopped() => println!(
-                "{} {} [{}]",
-                format!("@{}:", task.id),
-                task.name,
-                task.status_text(),
-            ),
-        }
-    }
-}
-
-fn print_tasks_long(tasks: HashMap<u32, timers::Task>) {
-    for id in tasks.keys().sorted() {
-        let task = &tasks[id];
-
-        match task.status() {
-            timers::TaskStatus::Logging() => println!(
-                "{} {}",
-                format!("@{}:", task.id).yellow().bold(),
-                task.name.red().bold(),
-            ),
-            timers::TaskStatus::Stopped() => println!(
-                "{} {}",
-                format!("@{}:", task.id),
-                task.name,
-            ),
-        }
-
-        println!("  status: {}", task.status_text());
-        println!("  duration: {}", timers::format_duration(task.duration()));
-        println!(
-            "  last log: {}",
-            task.logs.last().unwrap()
-                .start.with_timezone(&chrono::Local)
-                .format("%a %b %d %H:%M")
-        );
     }
 }
